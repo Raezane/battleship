d40566f1df0cfc7dc3ab809e8f95709a23ac7505
Antti Räsänen <143749752+Raezane@users.noreply.github.com>
2024-12-03 20:35:47 +0200

    create tests for the ship factory

---
+++ tests/ship.test.js
@@ -0,0 +1,37 @@
+import {ship} from '../src/models/ship';
+
+let testShip;
+
+beforeEach(() => {
+  testShip = ship(3);
+});
+
+describe('ship length tests', () => {
+  test('creates a ship of length 3', () => {
+    expect(testShip.getLength()).toBe(3);
+  });
+
+  test("ship's length is 4 at maximum", () => {
+    expect(testShip.getLength()).toBeLessThanOrEqual(4)
+  });
+
+  test("ship's length is 1 at minimum", () => {
+    expect(testShip.getLength()).toBeGreaterThanOrEqual(0)
+  });
+});
+
+describe('hit and sinking tests', () => {
+  test('hit is registered', () => {
+    testShip.hit();
+    expect(testShip.getHits()).toBe(1);
+  });
+
+  test('ship is sunk if hits are equal or more than ship length', () => {
+
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+
+    expect(testShip.isSunk()).toBe(true)
+  });
+});
